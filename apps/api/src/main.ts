eae351fe8451af6ef85f1931b3466541ff469110
SILAS-JOB <tiilas777@gmail.com>
2026-02-06 18:16:42 -0300

    feat: swagger docs

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,14 +1,17 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle("Tonho Api")
+    .setDescription("Api do workspace do Chicas Eventos e Tonho Locações")
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
